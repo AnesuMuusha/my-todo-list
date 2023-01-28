287783e55cb8172124c234dec8b5b8d95eebb082
AnesuMuusha <anesumuusha@gmail.com>
2023-01-28 20:41:00 +0200

    Adding todos to TodoList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React,{useState} from 'react';
-import Form from './components/Form';
-import './App.css';
-import TodoList from './components/TodoList';
+import React, { useState } from "react";
+import Form from "./components/Form";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const[inputText,setInputText]=useState("");
-  const[todos,setTodos] =  useState([]);
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>The 5am Club #to do list</h1>
       </header>
-      <Form setInputText={setInputText} inputText={inputText}todos={todos} setTodos={setTodos} />
-      <TodoList/>
+      <Form
+        setInputText={setInputText}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <TodoList todos={todos}/>
     </div>
   );
 }
