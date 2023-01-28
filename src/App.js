ef69e27293ea5380a09592fd97901316495db3a6
AnesuMuusha <anesumuusha@gmail.com>
2023-01-28 03:52:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Form from './components/Form';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
+  const[inputText,setInputText]=useState("");
+  const[todos,setTodos] =  useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>The 5am Club #to do list{inputText}</h1>
       </header>
+      <Form setInputText={setInputText} inputText={inputText}todos={todos} setTodos={setTodos} />
+      <TodoList/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+  return (
+    <div>
+      <form>
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button
+          onClick={submitTodoHandler}
+          className="todo-button"
+          type="submit"
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select className="filter-todo" name="todos">
+            <opton valvue="all">ALL</opton>
+            <opton valvue="completed">Completed</opton>
+            <opton valvue="uncompleted">UnCompleted</opton>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
