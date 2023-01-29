2eeafbcbd720db143dfd2673332a38151b265186
AnesuMuusha <anesumuusha@gmail.com>
2023-01-29 04:01:04 +0200

    added todo items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList =({todos})=>{
-    return(
-        <div className="todo-container">
-            <ul className="todo-list">
-                {todos.map((todo)=>(
-                    <Todo text={todo.text} id={todo.id}/>
-                ))}
-            </ul>
-        </div>
-    )
+const TodoList = ({ todos, setTodos,filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            text={todo.text}
+            key={todo.id}
+            setTodos={setTodos}
+            todos={todos}
+            todo={todo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default TodoList;
