692bfb3e2f4c4aef4d2d9ae678699e914e06a6d9
AnesuMuusha <anesumuusha@gmail.com>
2023-03-07 08:03:36 +0200

    change color onchange

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,50 @@
-import React from "react"
+import React, { useState } from "react";
 
-const Todo = ({text,todo,todos,setTodos}) => {
-    const deleteHandler=()=>{
-        setTodos(todos.filter((e1)=> e1.id !== todo.id));
-    };
-    const completeHandler = ()=>{
-        setTodos(todos.map(item =>{
-            if(item.id === todo.id){
-                return{
-                    ...item, completed:!item.completed
-            };
-        }
-        return item;
-        })
-        );
-    };
-
-    return(
-    <div className="todo">
-        <li className={`todo-item ${todo.completed? "completed": ""}`}>{text}</li>
-        <button className="complete-btn" onClick={completeHandler}>
-            <i className="fas fa-check"></i>
-        </button>
-        <button className="trash-btn" onClick={deleteHandler}>
-            <i className="fas fa-trash"></i>
-            </button>
-    </div>
-    )
+const buttonStyle = {
+  backgroundColor: "green",
+  color: "white",
+  border: "none",
+  padding: "17.5px",
+  cursor: "pointer",
 };
+
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const [color1, setColor] = useState(false);
+
+  const deleteHandler = () => {
+    setTodos(todos.filter((e1) => e1.id !== todo.id));
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+
+        return item;
+      })
+    );
+
+    setColor(!color1);
+  };
+
+  return (
+    <div className="todo">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button style={{ ...buttonStyle, backgroundColor: color1 ? "seagreen" : "gray" }} onClick={completeHandler}>
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="trash-btn" onClick={deleteHandler}>
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
+
 export default Todo;
