f24661982903bc5010e6f5685ebdb56bcc0dd5e1
AnesuMuusha <anesumuusha@gmail.com>
2023-03-14 12:27:33 +0200

    updated css styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,7 @@
-import React, { useState } from "react";
-
-const buttonStyle = {
-  backgroundColor: "green",
-  color: "white",
-  border: "none",
-  padding: "17.5px",
-  cursor: "pointer",
-};
+import React from "react";
 
 const Todo = ({ text, todo, todos, setTodos }) => {
-  const [color1, setColor] = useState(false);
-
+ 
   const deleteHandler = () => {
     setTodos(todos.filter((e1) => e1.id !== todo.id));
   };
@@ -28,8 +19,6 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         return item;
       })
     );
-
-    setColor(!color1);
   };
 
   return (
@@ -37,7 +26,8 @@ const Todo = ({ text, todo, todos, setTodos }) => {
       <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
         {text}
       </li>
-      <button style={{ ...buttonStyle, backgroundColor: color1 ? "seagreen" : "gray" }} onClick={completeHandler}>
+      <button className="complete-btn"
+ onClick={completeHandler}>
         <i className="fas fa-check"></i>
       </button>
       <button className="trash-btn" onClick={deleteHandler}>
